ca5d02cda545bac4c240a45b296ec1b8ce72a7bb
Kavinsky <nmunozfernandez@gmail.com>
2015-04-30 15:37:52 +0200

    ace_laserpointer to fhq_accesories

--- addons/fhq_accesories/CfgWeapons.hpp
+++ addons/fhq_accesories/CfgWeapons.hpp
@@ -4,12 +4,19 @@ class CfgWeapons {
         MRT_SwitchItemNextClass = "FHQ_acc_ANPEQ15_FlashLight";
         MRT_SwitchItemPrevClass = "FHQ_acc_ANPEQ15_FlashLight";
         MRT_SwitchItemHintText = "AN/PEQ15 - IR Laser";
+
+        ACE_nextModeClass = "FHQ_acc_ANPEQ15_FlashLight";
+        ACE_modeDescription = "AN/PEQ-15 - IR Laser";
     };
     class FHQ_acc_ANPEQ15_FlashLight: FHQ_acc_ANPEQ15 {
         descriptionShort = "$STR_A3_cfgWeapons_acc_flashlight1";
         MRT_SwitchItemNextClass = "FHQ_acc_ANPEQ15";
         MRT_SwitchItemPrevClass = "FHQ_acc_ANPEQ15";
         MRT_SwitchItemHintText = "AN/PEQ15 - Flashlight";
+
+        ACE_nextModeClass = "FHQ_acc_ANPEQ15";
+        ACE_modeDescription = "AN/PEQ-15 - Flashlight";
+
         MEU_HideItem = 1;
         class ItemInfo: InventoryFlashLightItem_Base_F {
             mass = 8;
@@ -45,12 +52,19 @@ class CfgWeapons {
         MRT_SwitchItemNextClass = "FHQ_acc_ANPEQ15_black_FlashLight";
         MRT_SwitchItemPrevClass = "FHQ_acc_ANPEQ15_black_FlashLight";
         MRT_SwitchItemHintText = "AN/PEQ15 - IR Laser";
+
+        ACE_nextModeClass = "FHQ_acc_ANPEQ15_black_FlashLight";
+        ACE_modeDescription = "AN/PEQ-15 - IR Laser";
     };
     class FHQ_acc_ANPEQ15_black_FlashLight: FHQ_acc_ANPEQ15_black {
         descriptionShort = "$STR_A3_cfgWeapons_acc_flashlight1";
         MRT_SwitchItemNextClass = "FHQ_acc_ANPEQ15_black";
         MRT_SwitchItemPrevClass = "FHQ_acc_ANPEQ15_black";
         MRT_SwitchItemHintText = "AN/PEQ15 - IR Flashlight";
+
+        ACE_nextModeClass = "FHQ_acc_ANPEQ15_black";
+        ACE_modeDescription = "AN/PEQ-15 - Flashlight";
+
         MEU_HideItem = 1;
         class ItemInfo: InventoryFlashLightItem_Base_F {
             mass = 8;
