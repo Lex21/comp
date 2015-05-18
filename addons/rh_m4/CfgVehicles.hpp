b2f42c539f9a532d630413b619e3255d57676fe1
Kavinsky <nmunozfernandez@gmail.com>
2015-05-18 05:10:47 +0200

    added rh_m4 comp

---
+++ addons/rh_m4/CfgVehicles.hpp
@@ -0,0 +1,36 @@
+class CfgVehicles {
+    class Box_MEU_Weapons;
+    class Box_MEU_Weapons_USMC: Box_MEU_Weapons {   
+        class TransportWeapons {
+            MACRO_ADDWEAPON(RH_m4,10);
+            MACRO_ADDWEAPON(RH_m4_m203,10);
+            MACRO_ADDWEAPON(RH_m4_ris,10);
+            MACRO_ADDWEAPON(RH_m4_ris_m203,10);
+            MACRO_ADDWEAPON(RH_m4_ris_m203s,10);
+            MACRO_ADDWEAPON(RH_m4a1_ris,10);
+            MACRO_ADDWEAPON(RH_m4a1_ris_m203,10);
+            MACRO_ADDWEAPON(RH_m4a1_ris_m203s,10);
+            MACRO_ADDWEAPON(RH_m4sbr,10);
+            MACRO_ADDWEAPON(RH_m4sbr_g,10);
+            MACRO_ADDWEAPON(RH_m4sbr_b,10);
+            MACRO_ADDWEAPON(RH_m16a4,10);
+            MACRO_ADDWEAPON(RH_m16a4gl,10);
+            MACRO_ADDWEAPON(RH_m16a4_m,10);
+            MACRO_ADDWEAPON(RH_mk12mod1,10);
+            MACRO_ADDWEAPON(RH_samr,10);
+            MACRO_ADDWEAPON(RH_m4m,10);
+            MACRO_ADDWEAPON(RH_m4m_g,10);
+            MACRO_ADDWEAPON(RH_m4m_b,10);
+            MACRO_ADDWEAPON(RH_m4_moe,10);
+            MACRO_ADDWEAPON(RH_m4_moe_g,10);
+            MACRO_ADDWEAPON(RH_m4_moe_b,10);
+            MACRO_ADDWEAPON(RH_m27iar_tg,10);
+            MACRO_ADDWEAPON(RH_hk416_wdl,10);
+            MACRO_ADDWEAPON(RH_hk416_des,10);
+            MACRO_ADDWEAPON(RH_hk416_tg,10);
+            MACRO_ADDWEAPON(RH_hk416c_wdl,10);
+            MACRO_ADDWEAPON(RH_hk416c_des,10);
+            MACRO_ADDWEAPON(RH_hk416c_tg,10);
+        };
+    };
+};
---
+++ addons/rh_m4/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"meu_main", "meu_ammoboxes", "meu_magazines", "RH_m4_cfg"};
+        author[] = {"11thMEU Dev Team"};
+        authorUrl = "http://11thmeu.es";
+        versionDesc = "11thMEU Addons";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
