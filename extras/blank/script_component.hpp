aad4573eeebe1e572cd3658808a0a03208ce6800
Kavinsky <nmunozfernandez@gmail.com>
2015-05-18 04:59:15 +0200

    removed more shit from blank

--- extras/blank/script_component.hpp
+++ extras/blank/script_component.hpp
@@ -1,12 +1,3 @@
 #define COMPONENT blank
 #include "\z\meu_comp\addons\main\script_mod.hpp"
-
-#ifdef DEBUG_ENABLED_BLANK
-    #define DEBUG_MODE_FULL
-#endif
-
-#ifdef DEBUG_SETTINGS_BLANK
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_BLANK
-#endif
-
 #include "\z\meu\addons\main\script_macros.hpp"
