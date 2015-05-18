f197ed390fced136d778f2ad78b851fdc4c5aea6
Kavinsky <nmunozfernandez@gmail.com>
2015-05-18 05:10:40 +0200

    added rh_pistol comp

---
+++ addons/rh_pistols/CfgVehicles.hpp
@@ -0,0 +1,29 @@
+class CfgVehicles {
+    class Box_MEU_Weapons;
+    class Box_MEU_Weapons_USMC: Box_MEU_Weapons {   
+        class TransportWeapons {
+            MACRO_ADDWEAPON(RH_m1911,10);
+            MACRO_ADDWEAPON(RH_kimber,10);
+            MACRO_ADDWEAPON(RH_kimber_nw,10);
+            MACRO_ADDWEAPON(RH_usp,10);
+            MACRO_ADDWEAPON(RH_uspm,10);
+            MACRO_ADDWEAPON(RH_m9,10);
+            MACRO_ADDWEAPON(RH_m9c,10);
+            MACRO_ADDWEAPON(RH_g18,10);
+            MACRO_ADDWEAPON(RH_g17,10);
+            MACRO_ADDWEAPON(RH_g19,10);
+            MACRO_ADDWEAPON(RH_g19t,10);
+            MACRO_ADDWEAPON(RH_p226,10);
+            MACRO_ADDWEAPON(RH_p226s,10);
+        };
+
+        class TransportMagazines {
+            MACRO_ADDMAGAZINE(RH_12Rnd_45cal_usp,100);
+            MACRO_ADDMAGAZINE(RH_7Rnd_45cal_m1911,100);
+            MACRO_ADDMAGAZINE(RH_15Rnd_9x19_M9,100);
+            MACRO_ADDMAGAZINE(RH_33Rnd_9x19_g18,100);
+            MACRO_ADDMAGAZINE(RH_17Rnd_9x19_g17,100);
+            MACRO_ADDMAGAZINE(RH_15Rnd_9x19_SIG,100);
+        };
+    };
+};
