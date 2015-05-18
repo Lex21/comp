fd4e7889003b73aaff07ace9d6f81be74d69c55a
Kavinsky <nmunozfernandez@gmail.com>
2015-05-18 15:01:04 +0200

    fix str errors for gpnvg18

--- addons/a3_gpnvg18/CfgWeapons.hpp
+++ addons/a3_gpnvg18/CfgWeapons.hpp
@@ -6,5 +6,12 @@ class CfgWeapons {
         ACE_NightVision_grain = 0.05;
         ACE_NightVision_blur = 0.005;
         ACE_NightVision_radBlur = 0.000;
+
+        // fix for str not found error on log        
+        descriptionUse = "L3 Panoramic GPNVG18";
+        class Library
+        {
+            libTextDesc = "L3 Panoramic GPNVG18";
+        };
     };
 };
