4751f5296220aedf73fdb75cea5b9db113398013
Kavinsky <nmunozfernandez@gmail.com>
2015-05-18 04:42:42 +0200

    added a3_gpnvg18 to ammoboxes

---
+++ addons/a3_gpnvg18/CfgVehicles.hpp
@@ -0,0 +1,15 @@
+class CfgVehicles {
+	class Box_MEU_Weapons;
+	class Box_MEU_Items_USMC: Box_MEU_Items {
+	    class TransportItems {
+	    	MACRO_ADDITEM(A3_GPNVG18_F,10);
+	    	MACRO_ADDITEM(A3_GPNVG18_BLK_F,10);
+	    	MACRO_ADDITEM(A3_GPNVG18b_F,10);
+	    	MACRO_ADDITEM(A3_GPNVG18b_BLK_F,10);
+	    	MACRO_ADDITEM(A3_GPNVG18_REC_F,10);
+	    	MACRO_ADDITEM(A3_GPNVG18_REC_BLK_F,10);
+	    	MACRO_ADDITEM(A3_GPNVG18b_REC_F,10);
+	    	MACRO_ADDITEM(A3_GPNVG18b_REC_BLK_F,10);
+	    };
+	};
+};
