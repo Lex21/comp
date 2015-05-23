335967b5de3b6294acddffc170ed196a21443422
Javier Rico <javi921@hotmail.es>
2015-05-23 19:23:39 +0200

    Added massi marsoc equipment configs for equipment module

---
+++ addons/mas_usa_mars/CfgMEUCommonEquipment.hpp
@@ -0,0 +1,23 @@
+class CfgMEUCommonEquipment {
+    class Backpacks {
+        classes[] += {
+            "B_mas_AssaultPack_mul",
+            "B_mas_AssaultPack_des",
+            "B_mas_AssaultPack_black",
+            "B_mas_AssaultPack_wint",
+            "B_mas_AssaultPack_rng",
+            "B_mas_Bergen_mul",
+            "B_mas_Bergen_des",
+            "B_mas_Bergen_black",
+            "B_mas_Bergen_wint",
+            "B_mas_Bergen_rng",
+            "O_mas_Bergen_flo",
+            "O_mas_Bergen_blk",
+            "B_mas_Kitbag_mul",
+            "B_mas_Kitbag_des",
+            "B_mas_Kitbag_black",
+            "B_mas_Kitbag_wint",
+            "B_mas_Kitbag_rng"
+        };
+    };
+};
